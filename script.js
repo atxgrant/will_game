320a6903bafe86dc6389bff371074be502e39723
Grant Heimbach <grantheimbach@gmail.com>
2025-05-23 08:12:48 -0500

    Update script.js
    Changing the color schemes that are available

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const colorSchemes = [
-    { backgroundColor: "#FFD700", fontColor: "#000000" }, // Gold background, Black font
-    { backgroundColor: "#000080", fontColor: "#FFFFFF" }, // Navy background, White font
-    { backgroundColor: "#E6E6FA", fontColor: "#333333" }, // Lavender background, Dark Gray font
-    { backgroundColor: "#2F4F4F", fontColor: "#FFFFFF" }, // Dark Slate Gray background, White font
-    { backgroundColor: "#FFFACD", fontColor: "#000000" }  // Lemon Chiffon background, Black font
+    { backgroundColor: "#0F198F", fontColor: "#10CCDA" }, // Navy background, Cyan font
+    { backgroundColor: "#7C21B7", fontColor: "#6AEB96" }, // Purple background, Lime font
+    { backgroundColor: "#10FBAE", fontColor: "#272D1F" }, // Cyan background, Black font
+    { backgroundColor: "#982209", fontColor: "#A1DDFD" }, // Red background, Light Blue font
+    { backgroundColor: "#5A57AC", fontColor: "#5EFCEF" }  // Lavendar background, Cyan font
 ];
 
 // Function to apply a random color scheme
